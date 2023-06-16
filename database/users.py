36fca5064c31d6129c3bd08c536903983861fb74
Kiran6412 <136492278+Kiran6412@users.noreply.github.com>
2023-06-16 18:14:37 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -18,7 +18,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "easysky.in",
+            "base_site": "tulinks.one",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
